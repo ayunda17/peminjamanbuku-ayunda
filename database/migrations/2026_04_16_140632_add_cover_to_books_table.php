14377eb8d3ec3c0db62344031915edf47bc89043
Tarisya Nurcahyani <tarisyanurcahyani73@gmail.com>
2026-04-20 08:35:03 +0700

    databse

--- database/migrations/2026_04_16_140632_add_cover_to_books_table.php
+++ database/migrations/2026_04_16_140632_add_cover_to_books_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('books', function (Blueprint $table) {
-            //
+            if (!Schema::hasColumn('books', 'cover')) {
+                $table->string('cover')->nullable();
+            }
         });
     }
 
@@ -22,7 +24,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('books', function (Blueprint $table) {
-            //
+            if (Schema::hasColumn('books', 'cover')) {
+                $table->dropColumn('cover');
+            }
         });
     }
 };
